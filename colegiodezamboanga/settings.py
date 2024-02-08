531b8603ae93ff86ff4ba2a0fa7ba5c2cfd9eeff
Manntala <manny.talaroc@gmail.com>
2024-02-08 22:40:26 +0800

    add key

--- colegiodezamboanga/settings.py
+++ colegiodezamboanga/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-*7!i#(pz87m5v3g4=7*7go((o%jl3ej=wdtz$xx8sf*jvl94z("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
