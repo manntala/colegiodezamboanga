d5bbff32400b69bcb8952910e0d34bfaefdccfde
Manntala <manny.talaroc@gmail.com>
2024-02-08 23:20:42 +0800

    debug false

--- colegiodezamboanga/settings.py
+++ colegiodezamboanga/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-*7!i#(pz87m5v3g4=7*7go((o%jl3ej=wdtz$xx8sf*jvl94z("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1' ,'.vercel.app']
 
